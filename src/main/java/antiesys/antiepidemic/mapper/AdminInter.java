9f7404c1f306bf4c747c39f6d11c6c6e328f4e7a
juhick <qhq1324@vip.qq.com>
2020-08-11 10:12:04 +0800

    修改Mapper的注释为JavaDOC

--- src/main/java/antiesys/antiepidemic/mapper/AdminInter.java
+++ src/main/java/antiesys/antiepidemic/mapper/AdminInter.java
@@ -8,6 +8,10 @@ import org.springframework.stereotype.Repository;
 @Repository
 @Mapper
 public interface AdminInter {
-    //查询一个管理员
+    /**
+     * 查询一个管理员
+     * @param adminId 管理员ID
+     * @return 查询到的管理员对象
+     */
     Manager SelectOne(@Param("adminId")int adminId);
 }
